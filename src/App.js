8c34618df73a4cc5e29e30e4e17f809374c5a5de
02arslon <arslonruziboyev@gmail.com>
2023-07-31 18:45:12 +0900

    changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      {" "}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
